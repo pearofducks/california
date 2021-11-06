ebff7a9ed332cc674d57e833f54641954413e787
Dave Honneffer <pearofducks@gmail.com>
2021-11-06 19:45:16 +0100

    More

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { getMonth, getDaysInMonth, startOfWeek, getWeeksInMonth, getDay, startOfMonth, endOfMonth, addDays, lightFormat } from 'date-fns'
+import { getMonth, getYear, getDaysInMonth, startOfWeek, getWeeksInMonth, getDay, startOfMonth, endOfMonth, addDays, lightFormat } from 'date-fns'
 
 const dayModel = new Date('December 1, 2021')
 const month = getMonth(dayModel)
+const year = getYear(dayModel)
 const start = startOfMonth(dayModel)
 const startDay = getDay(start)
 const end = endOfMonth(dayModel)
@@ -22,11 +23,14 @@ Array.from({ length: numberOfWeeks }).forEach(week => {
 
   monthModel.push(weekModel)
 })
+
+const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember']
 </script>
 
 <template>
   <main>
     <section style="max-width: 240px" class="bg-aqua-100">
+    <h4 class="text-center py-8 mb-0">{{ months[month] }} {{ year }}</h4>
     <div class="grid grid-cols-7 text-12 justify-items-center py-4">
       <p class="mb-0">ma</p>
       <p class="mb-0">ti</p>
@@ -37,8 +41,8 @@ Array.from({ length: numberOfWeeks }).forEach(week => {
       <p class="mb-0">sø</p>
     </div>
     <article v-for="week in monthModel" :key="week" class="grid grid-cols-7 text-14 justify-items-center">
-      <p class="mb-0" v-for="d in week" :key="d">
-        <span v-if="getMonth(d) === month">{{ lightFormat(d, 'dd') }}</span>
+      <p class="mb-0 tabular-nums p-8" v-for="d in week" :key="d">
+        <span v-if="getMonth(d) === month">{{ lightFormat(d, 'd') }}</span>
       </p>
     </article>
     </section>
