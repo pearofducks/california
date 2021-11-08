a0a2114422f8dec57175639e9c23f14bf93e040d
Dave Honneffer <pearofducks@gmail.com>
2021-11-08 09:07:12 +0100

    Add direction to transition

--- src/Month.vue
+++ src/Month.vue
@@ -2,7 +2,7 @@
   <section style="max-width: 240px">
       <!-- FIXME - flip when moving the other way -->
       <h4 class="text-center py-8 mb-0 relative overflow-hidden flex justify-center">
-        <transition name="tab">
+        <transition :name="changedForward ? 'tab' : 'tab-back'">
           <div class="transition-all" :key="monthName">{{ monthName }} {{ year }}</div>
         </transition>
       </h4>
@@ -17,7 +17,7 @@
 import WeekdayTitles from './WeekdayTitles.vue'
 import Day from './Day.vue'
 import { addMonths, getMonth, getYear, startOfWeek, getWeeksInMonth, startOfMonth, endOfMonth, addDays, lightFormat } from 'date-fns'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const props = defineProps({
   displayMonth: Object
@@ -37,21 +37,41 @@ const computeMonth = (firstOfMonth, weeksInMonth) => {
     })
   })
 }
+const changedForward = ref(false)
 
 const monthModel = computed(() => computeMonth(start.value, numberOfWeeks.value))
 const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember']
 const monthName = computed(() => months[month.value])
 const show = date => window.alert(date)
+
+const getChangedForward = (curr, prev) => {
+  if (curr == 11 && prev == 0) return false
+  if (curr == 0 && prev == 11) return true
+  return curr > prev
+}
+watch(month, (curr, prev) => {
+  changedForward.value = getChangedForward(curr, prev)
+})
 </script>
 
 <style>
-.tab-enter-from {
-  position: absolute;
-  transform: translateY(100%);
-}
+.tab-back-enter-from,
+.tab-back-leave-to,
+.tab-enter-from,
 .tab-leave-to {
   position: absolute;
   opacity: 0;
+}
+.tab-enter-from {
+  transform: translateY(-100%);
+}
+.tab-leave-to {
+  transform: translateY(100%);
+}
+.tab-back-enter-from {
+  transform: translateY(100%);
+}
+.tab-back-leave-to {
   transform: translateY(-100%);
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-  rollupOptions: {
-    external: ['vue']
-  }
+    rollupOptions: {
+      external: ['vue']
+    }
   }
 })
