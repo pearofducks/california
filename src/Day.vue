0349160285edbe14a77dd2880dc74912e71f7386
Dave Honneffer <pearofducks@gmail.com>
2021-11-19 10:46:31 +0100

    Add really stupid example of decorations

--- src/Day.vue
+++ src/Day.vue
@@ -8,12 +8,18 @@
 import { lightFormat, getMonth } from 'date-fns'
 import { computed } from 'vue'
 
-const props = defineProps({ date: Date, month: Number })
+const props = defineProps({ date: Date, month: Number, decorations: { type: Object, default: () => ({}) } })
+const decorations = computed(() => Object.entries(props.decorations).filter(([_, v]) => v).map(([k]) => k))
 
 const inCurrentMonth = computed(() => getMonth(props.date) === props.month)
 const buttonClasses = computed(() => ({
   ['mb-0 tabular-nums w-full text-center p-8 rounded-full']: true,
   ['pointer-events-none']: !inCurrentMonth.value,
-  ['hover:bg-gray-200 focus-ring cursor-pointer']: inCurrentMonth.value
+  ['hover:bg-gray-200 focus-ring cursor-pointer']: inCurrentMonth.value,
+  [decorations.value]: true
 }))
 </script>
+
+<style>
+.yup { background-color: red; }
+</style>
--- src/Month.vue
+++ src/Month.vue
@@ -8,7 +8,7 @@
       </h4>
     <weekday-titles />
     <article v-for="week in monthModel" :key="week" class="grid grid-cols-7 text-14 justify-items-center">
-      <day v-for="d in week" :key="d" :date="d" :month="month" @select="show" />
+      <day v-for="d in week" :key="d.date" :date="d.date" :month="month" @select="show" :decorations="d.decorations" />
     </article>
   </section>
 </template>
@@ -18,6 +18,7 @@ import WeekdayTitles from './WeekdayTitles.vue'
 import Day from './Day.vue'
 import { addMonths, getMonth, getYear, startOfWeek, getWeeksInMonth, startOfMonth, endOfMonth, addDays, lightFormat } from 'date-fns'
 import { computed, ref, watch } from 'vue'
+import { computeMonth } from '../logic.js'
 
 const props = defineProps({
   displayMonth: Object
@@ -27,19 +28,9 @@ const year = computed(() => getYear(props.displayMonth))
 const start = computed(() => startOfMonth(props.displayMonth))
 const numberOfWeeks = computed(() => getWeeksInMonth(props.displayMonth, { weekStartsOn: 1 }))
 
-const computeMonth = (firstOfMonth, weeksInMonth) => {
-  let dateIndex = startOfWeek(firstOfMonth, { weekStartsOn: 1 })
-  return Array.from({ length: weeksInMonth }, () => {
-    return Array.from({ length: 7 }, () => {
-      const date = dateIndex
-      dateIndex = addDays(dateIndex, 1)
-      return date
-    })
-  })
-}
 const changedForward = ref(false)
 
-const monthModel = computed(() => computeMonth(start.value, numberOfWeeks.value))
+const monthModel = computed(() => computeMonth(props.displayMonth, { decoratorFunction: (d) => ({ yup: d.getDay() == 2 }) }))
 const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember']
 const monthName = computed(() => months[month.value])
 const show = date => window.alert(date)
